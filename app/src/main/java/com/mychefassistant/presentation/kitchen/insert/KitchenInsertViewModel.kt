d1a01730a7f344e7f64a7ee99c1d94bcf6b4ac2a
Mehdi HosseinZade <arshen@outlook.com>
2021-05-01 15:52:33 +0430

    refactor kitchen insert fragment

--- app/src/main/java/com/mychefassistant/presentation/kitchen/insert/KitchenInsertViewModel.kt
+++ app/src/main/java/com/mychefassistant/presentation/kitchen/insert/KitchenInsertViewModel.kt
@@ -3,17 +3,26 @@ package com.mychefassistant.presentation.kitchen.insert
 import com.mychefassistant.core.domain.Kitchen
 import com.mychefassistant.framework.ChefAssistantViewModel
 import com.mychefassistant.framework.Interactors
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.runBlocking
 
 class KitchenInsertViewModel(private val interactors: Interactors) : ChefAssistantViewModel() {
-    fun addKitchen(title: String, icon: Int?, location: Int?) {
-        GlobalScope.launch {
-            withContext(Dispatchers.IO) {
-                interactors.addKitchen(Kitchen(title = title, icon = icon, location = location))
-            }
+    fun addKitchen(title: String, icon: Int?, location: Int?) = runBlocking {
+        if (title.isNullOrBlank()) {
+            setEvent(onCanNotEmptyTitle)
+            return@runBlocking
         }
+        val find = interactors.findKitchen(title, icon, location);
+        if (find.isNotEmpty()) {
+            setEvent(onKitchenExist, find[0].id)
+            return@runBlocking
+        }
+        interactors.addKitchen(Kitchen(title = title, icon = icon, location = location))
+        setEvent(onSuccessInsert)
+    }
+
+    companion object {
+        const val onCanNotEmptyTitle = "onCanNotEmptyTitle"
+        const val onKitchenExist = "onKitchenExist"
+        const val onSuccessInsert = "onSuccessInsert"
     }
 }
