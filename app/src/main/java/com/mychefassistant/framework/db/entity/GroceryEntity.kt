b8414e71714f74af27296b93561846759c46103f
Mehdi HosseinZade <arshen@outlook.com>
2021-05-05 13:18:06 +0430

    remove useless key in database

--- app/src/main/java/com/mychefassistant/framework/db/entity/GroceryEntity.kt
+++ app/src/main/java/com/mychefassistant/framework/db/entity/GroceryEntity.kt
@@ -1,6 +1,5 @@
 package com.mychefassistant.framework.db.entity
 
-import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
@@ -9,15 +8,11 @@ data class GroceryEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
 
-    @ColumnInfo(name = "kitchen")
     val kitchen: Int,
 
-    @ColumnInfo(name = "title")
     val title: String,
 
-    @ColumnInfo(name = "value")
     val value: String = "",
 
-    @ColumnInfo(name = "status")
     val status: Boolean = false
 )
